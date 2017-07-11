072fbaeb119149fe720a43c2f04257fc268fef9c
Idan Cohen <idan@idancohen.com>
2017-07-11 10:42:02 +0300

    cleanup

--- src/components/components.module.js
+++
@@ -1,9 +0,0 @@
-// import all components' modules
-import MainModule from './main/main.module';
-
-// Register module, inject components and export name
-export default angular
-  .module('app.components', [
-    MainModule,
-  ])
-  .name;
--- src/library/features.js
+++
@@ -1,7 +0,0 @@
-export function doSomething() {
-  return 'library is working!';
-}
-
-export function doSomethingElse() {
-  return 'library is working!';
-}
