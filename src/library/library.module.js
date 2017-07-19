ce81fff12b4af50feeb7108157b2ba1eee85b499
MenyChiko <tziko770@gmail.com>
2017-07-19 21:07:35 +0300

    create Tooltip directive #goodnessSquad

--- src/library/library.module.js
+++ src/library/library.module.js
@@ -8,12 +8,15 @@ import './library.scss';
 import TestModule from './test/test.module';
 import DropdownModule from './dropdown/dropdown.module';
 import TabsModule from './tabs/tabs.module';
+import TooltipModule from './tooltip/tooltip.module';
 
 // Register module, inject components and export name
 export default angular
-  .module('ng1bs4.library', [
-    TestModule,
-    DropdownModule,
-    TabsModule
-  ])
-  .name;
+    .module('ng1bs4.library', [
+        TestModule,
+        DropdownModule,
+        TabsModule,
+        TooltipModule
+
+    ])
+    .name;
