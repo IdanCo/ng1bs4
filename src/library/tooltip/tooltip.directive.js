8f6ce0bb9815be3ce4eeb22933ee02384bb569f3
MenyChiko <tziko770@gmail.com>
2017-07-19 21:07:35 +0300

    create Tooltip directive #goodnessSquad

---
+++ src/library/tooltip/tooltip.directive.js
@@ -0,0 +1,42 @@
+const DEFAULT_CONTENT = 'Please provide tooltip content';
+const DEFAULT_TRIGGER = 'hover focus';
+const DEFAULT_PLACEMENT = 'bottom';
+
+const TooltipDirective = ($compile, $timeout) => ({
+    restrict: 'A',
+    scope: {
+        bpTooltipContent: '@',
+        bpTooltipPlacement: '@',
+        bpTooltipTriger: '@'
+    },
+
+    link: function($scope, $element, $attrs) {
+        // set a base object for tooltip options
+        let options = {
+            trigger: $scope.bpTooltipTriger || DEFAULT_TRIGGER,
+            animation: true,
+            html: true,
+            title: getContent(),
+            placement: $scope.bpTooltipPlacement || DEFAULT_PLACEMENT,
+            delay: { "show": 200, "hide": 0 },
+        };
+
+        initTooltip();
+
+        function getContent() {
+            if ($scope.bpTooltipContent) {
+                return $scope.bpTooltipContent;
+            } else {
+                return DEFAULT_CONTENT
+            }
+        }
+
+        function initTooltip() {
+            $element.removeAttr('ngbs-tooltip');
+            $element.tooltip(options);
+        }
+
+    }
+});
+
+export default TooltipDirective;
