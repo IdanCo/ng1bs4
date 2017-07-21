ec6282e94699133050f2830bfd80d745d6c0ec22
Idan Cohen <idan@idancohen.com>
2017-07-22 02:17:33 +0300

    imporve lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,8 +12,6 @@ module.exports = {
     "semi": [2, "always"], // end lines with semicolon
     "eol-last": 2, // end files with newline
     "indent": [2, 2], // indent with two spaces
-    // new ones
     "angular/file-name": 0 // not friendly to modular architecture
-
   }
 };
