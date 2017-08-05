6dc7d52bfc5265201fe904d4f05ac90777cce93e
Langdon Oliver <langdon@gmail.com>
2017-08-05 15:25:26 -0700

    dropped ng-init jank

--- src/docs/demos/progress/progress-demo.controller.js
+++ src/docs/demos/progress/progress-demo.controller.js
@@ -1,1 +1,9 @@
-export default class {}
+export default class {
+  constructor() {
+    this.background = '';
+    this.height = 20;
+    this.label = '';
+    this.max = 100;
+    this.value = 25;
+  }
+}
